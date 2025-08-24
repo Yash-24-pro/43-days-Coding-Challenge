3ca0f2fbca22fb96e6e62fd37eb256f74e319332
Yash Tripathi <yashtripathi2400@gmail.com>
2025-08-24 18:27:50 +0530

    Add files via upload

---
+++ CodeFOrces344A.cpp
@@ -0,0 +1,28 @@
+
+/*The first line of the input contains an integer n (1 ≤ n ≤ 100000) — the number of magnets. Then n lines follow. The i-th line (1 ≤ i ≤ n) contains either characters "01", if Mike put the i-th magnet in the "plus-minus" position, or characters "10", if Mike put the magnet in the "minus-plus" position.
+
+Output
+On the single line of the output print the number of groups of magnets.*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    string a[n];
+    int pair = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        cout << endl;
+    }
+    for (int j = 0; j < n; j++)
+    {
+        if (a[j] != a[j + 1])
+        {
+            pair++;
+        }
+    }
+    cout<<pair;
+    return 0;
+}
