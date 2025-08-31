13c91fdf1b37f246183951f1e3c5abbcb02b7f7f
Yash Tripathi <yashtripathi2400@gmail.com>
2025-08-31 11:10:29 +0530

    Add files via upload

---
+++ CodeForces122A.cpp
@@ -0,0 +1,30 @@
+/*Petya loves lucky numbers. Everybody knows that lucky numbers are positive integers whose decimal representation contains only the lucky digits 4 and 7. For example, numbers 47, 744, 4 are lucky and 5, 17, 467 are not.
+
+Petya calls a number almost lucky if it could be evenly divided by some lucky number. Help him find out if the given number n is almost lucky.
+
+Input
+The single line contains an integer n (1 ≤ n ≤ 1000) — the number that needs to be checked.
+
+Output
+In the only line print "YES" (without the quotes), if number n is almost lucky. Otherwise, print "NO" (without the quotes).*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[15] = {4, 44, 444, 7, 77, 777, 47, 477, 74, 744, 474, 447, 744, 747, 774};
+    for (int i = 0; i < 15; i++)
+    {
+        if (n % arr[i] == 0)
+        {
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+
+    return 0;
+}
