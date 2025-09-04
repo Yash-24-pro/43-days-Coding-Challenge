39be72beb8026530c9734a348188a4d9af0ce7de
Yash Tripathi <yashtripathi2400@gmail.com>
2025-09-04 18:08:34 +0530

    Update CodeForces486A.cpp

--- CodeForces486A.cpp
+++ CodeForces486A.cpp
@@ -1,3 +1,16 @@
+/*For a positive integer n let's define a function f:
+
+f(n) =  - 1 + 2 - 3 + .. + ( - 1)nn
+
+Your task is to calculate f(n) for a given integer n.
+
+Input
+The single line contains the positive integer n (1 ≤ n ≤ 1015).
+
+Output
+Print f(n) in a single line.*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -15,4 +28,5 @@ int main()
     }
 
     return 0;
+
 }
